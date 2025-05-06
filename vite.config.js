731c603d58d96c6129016c9c6a941b7eac03c4a0
Manideep <65816034+manideep1996@users.noreply.github.com>
2025-05-06 04:12:19 +0000

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// At the top of your vite.config.js or entry file:
+if (typeof globalThis.crypto === 'undefined') {
+  globalThis.crypto = require('crypto').webcrypto;
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
